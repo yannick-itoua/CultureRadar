ee10d23a6222dfc4bc6eb86e2d5a8debe649b116
yannick-itoua <mangeyannick@gmail.com>
2025-06-03 20:52:42 -0600

    first step

---
+++ cultureradar-backend/src/main/java/com/cultureradar/CultureradarApplication.java
@@ -0,0 +1,13 @@
+package com.cultureradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultureradarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultureradarApplication.class, args);
+	}
+
+}
---
+++ cultureradar-backend/src/test/java/com/cultureradar/CultureradarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cultureradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultureradarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
